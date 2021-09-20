93340e680e749aa885b97a1dccea66b5c891e906
Mehedi Hassan Sazol <mhsazol111@gmail.com>
2021-09-21 02:59:53 +0600

    design: Home page animation updated. Need some more tweaking.

--- custom-elm.js
+++ custom-elm.js
@@ -1,16 +1,24 @@
-// (function (history) {
-//   var pushState = history.pushState;
-//   history.pushState = function (state) {
-//     // YOUR CUSTOM HOOK / FUNCTION
-//     console.log('I am called from pushStateHook', state, history, arguments);
-//     window.location.reload();
-//     return pushState.apply(history, arguments);
-//   };
-// })(window.history);
-
 (function ($) {
+  var cssId = 'myCss'; // you could encode the css path itself to generate id..
+  if (!document.getElementById(cssId)) {
+    var head = document.getElementsByTagName('head')[0];
+    var link = document.createElement('link');
+    link.id = cssId;
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    link.href = 'https://pauli81.wpengine.com/wp-content/themes/hungry-house/custom-elm.css';
+    link.media = 'all';
+    head.appendChild(link);
+  }
+
+  $('body').append(`<div class="loading-screen-wrapper">Loading</div>`);
+
+  $(window).on('load', function () {
+    $('.loading-screen-wrapper').fadeOut();
+  });
+
   $(document).ready(function () {
-    console.log('on ready script');
+    // console.log('on ready script');
 
     (function (history) {
       var pushState = history.pushState;
@@ -20,7 +28,7 @@
         }
         // whatever else you want to do
         // maybe call onhashchange e.handler
-        console.log('on function call', arguments);
+        // console.log('on function call', arguments);
 
         $('body').append(`<div class="loading-screen-wrapper">Loading</div>`);
 
@@ -39,19 +47,131 @@
     };
 
     setTimeout(() => {
-      var cssId = 'myCss'; // you could encode the css path itself to generate id..
-      if (!document.getElementById(cssId)) {
-        var head = document.getElementsByTagName('head')[0];
-        var link = document.createElement('link');
-        link.id = cssId;
-        link.rel = 'stylesheet';
-        link.type = 'text/css';
-        link.href = 'https://pauli81.wpengine.com/wp-content/themes/hungry-house/custom-elm.css';
-        link.media = 'all';
-        head.appendChild(link);
-      }
-
       const oldAboutSection = $('#jLvMhP');
+
+      const animateBanner = $(`
+      <div class="sq-section section-1">
+        <div class="container">
+          <div class="hover-thumb-wrapper" id="woldy-thumb">
+            <img src="https://pauli81.wpengine.com/wp-content/themes/hungry-house/img/products/woldy-thumb.png" alt="woldy" />
+          </div>
+          <div class="hover-thumb-wrapper" id="sermon-thumb">
+            <img src="https://pauli81.wpengine.com/wp-content/themes/hungry-house/img/products/sermon-thumb.png" alt="sermon" />
+          </div>
+          <div class="hover-thumb-wrapper" id="apocalypse-thumb">
+            <img src="https://pauli81.wpengine.com/wp-content/themes/hungry-house/img/products/apocalypse-thumb.png" alt="apocalypse" />
+          </div>
+          <div class="hover-thumb-wrapper" id="house-thumb">
+            <img src="https://pauli81.wpengine.com/wp-content/themes/hungry-house/img/products/house-thumb.png" alt="house" />
+          </div>
+          <div class="hover-thumb-wrapper" id="good-thumb">
+            <img src="https://pauli81.wpengine.com/wp-content/themes/hungry-house/img/products/good-thumb.png" alt="good" />
+          </div>
+        </div>
+        <nav class="menu">
+          <div class="menu__item" data-id="woldy-thumb">
+            <div class="container">
+              <a class="menu__item-link" href="#"><span class="font-sharp">Woldy Kusina</span> <span class="font-palmdale">modern filipino</span></a>
+            </div>
+            <div class="marquee">
+              <div class="marquee__inner-wrap">
+                <div class="marquee__inner" aria-hidden="true">
+                  <span class="font-sharp">Woldy Kusina</span> <span class="star"><img src="https://pauli81.wpengine.com/wp-content/themes/hungry-house/img/star.svg" alt="star"/></span>
+                  <span class="font-palmdale">Order Now</span> <span class="star"><img src="https://pauli81.wpengine.com/wp-content/themes/hungry-house/img/star.svg" alt="star"/></span>
+                  <span class="font-sharp">Dom Dom</span> <span class="star"><img src="https://pauli81.wpengine.com/wp-content/themes/hungry-house/img/star.svg" alt="star"/></span>
+                  <span class="font-palmdale">Order Now</span> <span class="star"><img src="https://pauli81.wpengine.com/wp-content/themes/hungry-house/img/star.svg" alt="star"/></span>
+                  <span class="font-sharp">Santa Maria</span> <span class="star"><img src="https://pauli81.wpengine.com/wp-content/themes/hungry-house/img/star.svg" alt="star"/></span>
+                  <span class="font-palmdale">Order Now</span> <span class="star"><img src="https://pauli81.wpengine.com/wp-content/themes/hungry-house/img/star.svg" alt="star"/></span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="menu__item" data-id="sermon-thumb">
+            <div class="container">
+              <a class="menu__item-link" href="#"><span class="font-sharp">The Food Sermon</span> <span class="font-palmdale">caribbean inspired</span></a>
+            </div>
+            <div class="marquee">
+              <div class="marquee__inner-wrap">
+                <div class="marquee__inner" aria-hidden="true">
+                  <span class="font-sharp">Woldy Kusina</span> <span class="star"><img src="https://pauli81.wpengine.com/wp-content/themes/hungry-house/img/star.svg" alt="star"/></span>
+                  <span class="font-palmdale">Order Now</span> <span class="star"><img src="https://pauli81.wpengine.com/wp-content/themes/hungry-house/img/star.svg" alt="star"/></span>
+                  <span class="font-sharp">Dom Dom</span> <span class="star"><img src="https://pauli81.wpengine.com/wp-content/themes/hungry-house/img/star.svg" alt="star"/></span>
+                  <span class="font-palmdale">Order Now</span> <span class="star"><img src="https://pauli81.wpengine.com/wp-content/themes/hungry-house/img/star.svg" alt="star"/></span>
+                  <span class="font-sharp">Santa Maria</span> <span class="star"><img src="https://pauli81.wpengine.com/wp-content/themes/hungry-house/img/star.svg" alt="star"/></span>
+                  <span class="font-palmdale">Order Now</span> <span class="star"><img src="https://pauli81.wpengine.com/wp-content/themes/hungry-house/img/star.svg" alt="star"/></span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="menu__item" data-id="apocalypse-thumb">
+            <div class="container">
+              <a class="menu__item-link" href="#"><span class="font-sharp">Apocalypse Burger</span> <span class="font-palmdale">burgers to die for</span></a>
+            </div>
+            <div class="marquee">
+              <div class="marquee__inner-wrap">
+                <div class="marquee__inner" aria-hidden="true">
+                  <span class="font-sharp">Woldy Kusina</span> <span class="star"><img src="https://pauli81.wpengine.com/wp-content/themes/hungry-house/img/star.svg" alt="star"/></span>
+                  <span class="font-palmdale">Order Now</span> <span class="star"><img src="https://pauli81.wpengine.com/wp-content/themes/hungry-house/img/star.svg" alt="star"/></span>
+                  <span class="font-sharp">Dom Dom</span> <span class="star"><img src="https://pauli81.wpengine.com/wp-content/themes/hungry-house/img/star.svg" alt="star"/></span>
+                  <span class="font-palmdale">Order Now</span> <span class="star"><img src="https://pauli81.wpengine.com/wp-content/themes/hungry-house/img/star.svg" alt="star"/></span>
+                  <span class="font-sharp">Santa Maria</span> <span class="star"><img src="https://pauli81.wpengine.com/wp-content/themes/hungry-house/img/star.svg" alt="star"/></span>
+                  <span class="font-palmdale">Order Now</span> <span class="star"><img src="https://pauli81.wpengine.com/wp-content/themes/hungry-house/img/star.svg" alt="star"/></span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="menu__item" data-id="house-thumb">
+            <div class="container">
+              <a class="menu__item-link" href="#"><span class="font-sharp">House Specials</span> <span class="font-palmdale">your everydays</span></a>
+            </div>
+            <div class="marquee">
+              <div class="marquee__inner-wrap">
+                <div class="marquee__inner" aria-hidden="true">
+                  <span class="font-sharp">Woldy Kusina</span> <span class="star"><img src="https://pauli81.wpengine.com/wp-content/themes/hungry-house/img/star.svg" alt="star"/></span>
+                  <span class="font-palmdale">Order Now</span> <span class="star"><img src="https://pauli81.wpengine.com/wp-content/themes/hungry-house/img/star.svg" alt="star"/></span>
+                  <span class="font-sharp">Dom Dom</span> <span class="star"><img src="https://pauli81.wpengine.com/wp-content/themes/hungry-house/img/star.svg" alt="star"/></span>
+                  <span class="font-palmdale">Order Now</span> <span class="star"><img src="https://pauli81.wpengine.com/wp-content/themes/hungry-house/img/star.svg" alt="star"/></span>
+                  <span class="font-sharp">Santa Maria</span> <span class="star"><img src="https://pauli81.wpengine.com/wp-content/themes/hungry-house/img/star.svg" alt="star"/></span>
+                  <span class="font-palmdale">Order Now</span> <span class="star"><img src="https://pauli81.wpengine.com/wp-content/themes/hungry-house/img/star.svg" alt="star"/></span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="menu__item" data-id="good-thumb">
+            <div class="container">
+              <a class="menu__item-link" href="#"><span class="font-sharp">The Goods Mart</span> <span class="font-palmdale">snacks & drinks</span></a>
+            </div>
+            <div class="marquee">
+              <div class="marquee__inner-wrap">
+                <div class="marquee__inner" aria-hidden="true">
+                  <span class="font-sharp">Woldy Kusina</span> <span class="star"><img src="https://pauli81.wpengine.com/wp-content/themes/hungry-house/img/star.svg" alt="star"/></span>
+                  <span class="font-palmdale">Order Now</span> <span class="star"><img src="https://pauli81.wpengine.com/wp-content/themes/hungry-house/img/star.svg" alt="star"/></span>
+                  <span class="font-sharp">Dom Dom</span> <span class="star"><img src="https://pauli81.wpengine.com/wp-content/themes/hungry-house/img/star.svg" alt="star"/></span>
+                  <span class="font-palmdale">Order Now</span> <span class="star"><img src="https://pauli81.wpengine.com/wp-content/themes/hungry-house/img/star.svg" alt="star"/></span>
+                  <span class="font-sharp">Santa Maria</span> <span class="star"><img src="https://pauli81.wpengine.com/wp-content/themes/hungry-house/img/star.svg" alt="star"/></span>
+                  <span class="font-palmdale">Order Now</span> <span class="star"><img src="https://pauli81.wpengine.com/wp-content/themes/hungry-house/img/star.svg" alt="star"/></span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </nav>
+        <div class="container">
+          <a href="/" class="view-full-menu font-sharp">View Full Menu</a>
+        </div>
+      </div>
+      `);
+      oldAboutSection.prepend(animateBanner);
+
+      $('body').on('mouseenter', '.menu__item', function (e) {
+        const id = $(this).data('id');
+        $('.hover-thumb-wrapper').removeClass('hover-active');
+        $(`#${id}`).addClass('hover-active');
+      });
+
+      $('body').on('mouseleave', '.menu__item', function (e) {
+        $('.hover-thumb-wrapper').removeClass('hover-active');
+      });
+
       const newAboutSection = $(`
       <div class="sq-section section-2">
         <div class="container">
@@ -66,7 +186,7 @@
       `);
       oldAboutSection.append(newAboutSection);
 
-      const oldInstagramSection = $('#HgjPAk');
+      // const oldInstagramSection = $('#HgjPAk');
 
       const oldNewsLetter = $('#yeCMuv');
       const newsletterBtn = $('#yeCMuv .action-button__button');
