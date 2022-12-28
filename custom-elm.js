0d5e5f321fa816e4045a2ff211900cde5ea55cce
Mehedi Hassan Sazol <mhsazol111@gmail.com>
2022-12-28 15:30:41 +0600

    fix: A menu item removed from the site.

--- custom-elm.js
+++ custom-elm.js
@@ -284,12 +284,12 @@ window.onload = function () {
           </div>
           <div class="footer-menu">
             <div class="footer-left-menu">
-              <a href="/baked-by-melissa">Baked by Melissa feat. Bowery</a>
               <a href="/woldy-kusina">Woldy Kusina</a>
               <a href="/chile-con-miel">Chile Con Miel</a>
               <a href="/apocalypse-burger">Apocalypse Burger</a>
               <a href="/caffe-panna">Caffe Panna</a>
               <a href="/the-goods-mart">The Goods Mart</a>
+              <a href="/house-specials">House Specials</a>
             </div>
             <div class="footer-right-menu">
               <div class="footer-right-menu-items">
@@ -367,33 +367,6 @@ window.onload = function () {
                 </div>
                 <div>
                   <span class="font-sharp">Chile Con Miel</span> <span class="star"><img src="https://envira57dev.wpengine.com/wp-content/themes/hungry-house/img/star.svg" alt="star"/></span>
-                  <span class="font-palmdale">Order Now</span> <span class="star"><img src="https://envira57dev.wpengine.com/wp-content/themes/hungry-house/img/star.svg" alt="star"/></span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="menu__item">
-          <div class="container" data-id="melissa-thumb">
-            <a class="menu__item-link" href="https://orderhungryhouse.square.site/baked-by-melissa"><span class="font-sharp">Baked by Melissa <span class="font-palmdale">feat. Bowery</span></span></a>
-          </div>
-          <div class="marquee">
-            <div class="marquee__inner-wrap">
-              <div class="marquee__inner" aria-hidden="true">
-                <div>
-                  <span class="font-sharp">Baked by Melissa</span> <span class="star"><img src="https://envira57dev.wpengine.com/wp-content/themes/hungry-house/img/star.svg" alt="star"/></span>
-                  <span class="font-palmdale">Order Now</span> <span class="star"><img src="https://envira57dev.wpengine.com/wp-content/themes/hungry-house/img/star.svg" alt="star"/></span>
-                </div>
-                <div>
-                  <span class="font-sharp">Baked by Melissa</span> <span class="star"><img src="https://envira57dev.wpengine.com/wp-content/themes/hungry-house/img/star.svg" alt="star"/></span>
-                  <span class="font-palmdale">Order Now</span> <span class="star"><img src="https://envira57dev.wpengine.com/wp-content/themes/hungry-house/img/star.svg" alt="star"/></span>
-                </div>
-                <div>
-                  <span class="font-sharp">Baked by Melissa</span> <span class="star"><img src="https://envira57dev.wpengine.com/wp-content/themes/hungry-house/img/star.svg" alt="star"/></span>
-                  <span class="font-palmdale">Order Now</span> <span class="star"><img src="https://envira57dev.wpengine.com/wp-content/themes/hungry-house/img/star.svg" alt="star"/></span>
-                </div>
-                <div>
-                  <span class="font-sharp">Baked by Melissa</span> <span class="star"><img src="https://envira57dev.wpengine.com/wp-content/themes/hungry-house/img/star.svg" alt="star"/></span>
                   <span class="font-palmdale">Order Now</span> <span class="star"><img src="https://envira57dev.wpengine.com/wp-content/themes/hungry-house/img/star.svg" alt="star"/></span>
                 </div>
               </div>
@@ -639,16 +612,16 @@ window.onload = function () {
       fixInstagramSliderWidth('lmSrHC', 3, '@chileconmiel', 'https://www.instagram.com/chileconmiel/', 'https://www.tiktok.com/@orderhungryhouse');
 
       // Baked by Melissa
-      primaryBannerContent('eXHEyW');
-      addMustHaveTitle('GQbMoc', `Pierce’s`);
-      fixSliderWidth('GQbMoc', 3);
-      addAboutSliderTitle('kbHKcR', `Baked by Melissa`);
-      addAboutSliderDetails(
-        'kbHKcR',
-        `Melissa Ben-Ishay is the co-founder and CEO of Baked by Melissa, a New York-based dessert company. Her food philosophy is simple: If you get your fulfillment from mealtime, you can absolutely enjoy dessert every day. Through TikTok, social media, and the Baked by Melissa blog, Melissa has inspired her community of more than 2 million followers to create delicious, nourishing meals. Each and every one of her recipes is designed to make people happy, one little bite at a time.`
-      );
-      fixNewsletterSection('WfUixP');
-      fixInstagramSliderWidth('zrlCvD', 3, '@bakedbymelissa', 'https://www.instagram.com/bakedbymelissa', 'https://www.tiktok.com/@orderhungryhouse');
+      // primaryBannerContent('eXHEyW');
+      // addMustHaveTitle('GQbMoc', `Pierce’s`);
+      // fixSliderWidth('GQbMoc', 3);
+      // addAboutSliderTitle('kbHKcR', `Baked by Melissa`);
+      // addAboutSliderDetails(
+      //   'kbHKcR',
+      //   `Melissa Ben-Ishay is the co-founder and CEO of Baked by Melissa, a New York-based dessert company. Her food philosophy is simple: If you get your fulfillment from mealtime, you can absolutely enjoy dessert every day. Through TikTok, social media, and the Baked by Melissa blog, Melissa has inspired her community of more than 2 million followers to create delicious, nourishing meals. Each and every one of her recipes is designed to make people happy, one little bite at a time.`
+      // );
+      // fixNewsletterSection('WfUixP');
+      // fixInstagramSliderWidth('zrlCvD', 3, '@bakedbymelissa', 'https://www.instagram.com/bakedbymelissa', 'https://www.tiktok.com/@orderhungryhouse');
 
       // Caffe Panna
       primaryBannerContent('ACabie');
@@ -775,7 +748,7 @@ window.onload = function () {
 
       // View all page
       viewAllSectionTitle('wjMzdC', 'Chile Con Miel', 'super tortas', 'https://orderhungryhouse.com/chile-con-miel');
-      viewAllSectionTitle('MjdBFv', 'Baked by Melissa', 'viral bowls & salads', 'https://orderhungryhouse.com/baked-by-melissa');
+      // viewAllSectionTitle('MjdBFv', 'Baked by Melissa', 'viral bowls & salads', 'https://orderhungryhouse.com/baked-by-melissa');
       viewAllSectionTitle('BcJLoX', 'Caffe Panna', 'ice cream', 'https://orderhungryhouse.com/caffe-panna');
       viewAllSectionTitle('qnYtAa', 'Woldy Kusina', 'modern filipino', 'https://orderhungryhouse.square.site/woldy-kusina');
       viewAllSectionTitle('MlUWwE', 'The Food Sermon', 'caribbean-inspired', 'https://orderhungryhouse.square.site/the-food-sermon');
